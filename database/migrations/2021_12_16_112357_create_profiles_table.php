80d8aacd24a69a3106c2c8f9d73be1110a24419a
mohp <miho.kj8.s@gmail.com>
2021-12-16 11:51:06 +0000

    課題14

---
+++ database/migrations/2021_12_16_112357_create_profiles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateprofilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+     // title と body と image_path を追記
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->string('name'); //名前
+            $table->string('gender'); // 性別
+            $table->string('hobby');  // 趣味
+            $table->string('introduction')->nullable();  // 自己紹介
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+}
